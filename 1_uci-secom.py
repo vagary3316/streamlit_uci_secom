fe74f699ec04b416b6060a416c21b44551df3ea8
Selina <selina.chen@mdfinstruments.com>
2024-12-29 22:34:13 +0800

    Try subheader

--- 1_uci-secom.py
+++ 1_uci-secom.py
@@ -22,8 +22,8 @@ Kaggle - UCI-SECOM Dataset
 (https://www.kaggle.com/datasets/paresh2047/uci-semcom/code?datasetId=28901&sortBy=commentCount)
 """)
 
+st.subheader(":bulb: Data Description:")
 st.markdown("""
-:bulb: Data Description:
 1567  semi-conductor manufacturing data examples with 591 features
 The last columns ['Pass/Fail'] is the target column: -1 means Pass; 1 means Fail
 And total 41951 NA in the dataset.
