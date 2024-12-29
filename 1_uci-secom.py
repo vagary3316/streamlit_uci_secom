fbb9d48cf1c8791a3c3b70f6eeb2dc9a25326c0d
Selina <selina.chen@mdfinstruments.com>
2024-12-29 19:54:17 +0800

    Data Description

--- 1_uci-secom.py
+++ 1_uci-secom.py
@@ -20,6 +20,7 @@ st.header('Data Analysis for the dataset - UCI-SECOM form Kaggle.')
 st.text("""
 Kaggle - UCI-SECOM Dataset
 (https://www.kaggle.com/datasets/paresh2047/uci-semcom/code?datasetId=28901&sortBy=commentCount)
+
 Data Description:
 1567  semi-conductor manufacturing data examples with 591 features
 The last columns ['Pass/Fail'] is the target column: -1 means Pass; 1 means Fail
