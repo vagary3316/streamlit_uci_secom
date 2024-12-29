511579aacf2729fb048c0be4435a3323e259e61d
Selina <selina.chen@mdfinstruments.com>
2024-12-29 22:28:50 +0800

    Adjust text for icon

--- 1_uci-secom.py
+++ 1_uci-secom.py
@@ -17,12 +17,12 @@ st.set_page_config(layout="wide",
 
 #Streamlit Header and Subheader
 st.header('Data Analysis for the dataset - UCI-SECOM form Kaggle.')
-st.text("""
+st.markdown("""
 Kaggle - UCI-SECOM Dataset
 (https://www.kaggle.com/datasets/paresh2047/uci-semcom/code?datasetId=28901&sortBy=commentCount)
 """)
 
-st.text("""
+st.markdown("""
 :bulb: Data Description:
 1567  semi-conductor manufacturing data examples with 591 features
 The last columns ['Pass/Fail'] is the target column: -1 means Pass; 1 means Fail
@@ -33,22 +33,23 @@ And total 41951 NA in the dataset.
 st.dataframe(secom)
 
 ## Explain NAs, and will replace NA with 0
-st.text("""
+st.markdown("""
 :bulb: Deal with NA:
 Replace with 0
 The values are not present, may mean that there is no signal detected for the sensor.
 """)
-# code
+
 secom.replace(np.nan, 0)
 
 ## Present the original Pass rate of the secom dataset
-st.text("""
+st.markdown("""
 :bulb: Pie Chart of the Pass/Fails 
 
 """)
-yield_df = secom['Pass/Fail'].value_counts()
 
+yield_df = secom['Pass/Fail'].value_counts()
 pie_for_yield = px.pie(yield_df, values='count', title='Pass/Fail Distribution', color_discrete_map='Pastel')
 pie_for_yield.update_traces(textposition='inside', textinfo='percent+label')
+
 st.plotly_chart(pie_for_yield, use_container_width=True)
 
