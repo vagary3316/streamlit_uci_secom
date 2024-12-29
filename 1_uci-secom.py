6d0036751d6032d9600e78ba5ae69480ce06faec
Selina <selina.chen@mdfinstruments.com>
2024-12-29 18:27:29 +0800

    Adjust Info

--- 1_uci-secom.py
+++ 1_uci-secom.py
@@ -10,4 +10,9 @@ st.set_page_config(layout="wide",
                    page_icon=":Chart:")
 
 #streamlit title&Time Frame
-st.title('Data Analysis for the dataset - UCI-SECOM form Kaggle.')
+st.header('Data Analysis for the dataset - UCI-SECOM form Kaggle.')
+st.caption("""
+Hi, this is my data visualization project.
+The data comes from Kaggle - UCI-SECOM Dataset
+link: https://www.kaggle.com/datasets/paresh2047/uci-semcom/code?datasetId=28901&sortBy=commentCount
+""")
