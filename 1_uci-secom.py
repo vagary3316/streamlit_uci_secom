45eba87366bd682869f25fd841cc55cbed5a7e14
Selina <selina.chen@mdfinstruments.com>
2024-12-29 21:57:10 +0800

    DataFrame

--- 1_uci-secom.py
+++ 1_uci-secom.py
@@ -7,12 +7,12 @@ from streamlit_gsheets import GSheetsConnection
 
 ## connect data from google sheet
 conn = st.connection("gsheets", type=GSheetsConnection)
-df = conn.read()
+secom = conn.read()
 
 
 ## streamlit_setting
 st.set_page_config(layout="wide",
-                   page_title="UCI-SECOM Dataset from Kaggle",
+                   page_title="UCI SECOM Dataset from Kaggle",
                    page_icon=":Chart:")
 
 #Streamlit Header and Subheader
@@ -22,8 +22,10 @@ Kaggle - UCI-SECOM Dataset
 (https://www.kaggle.com/datasets/paresh2047/uci-semcom/code?datasetId=28901&sortBy=commentCount)
 """)
 
-st.subheader("""
-Data Description:
+st.text("""
+:bulb:Data Description:
 1567  semi-conductor manufacturing data examples with 591 features
 The last columns ['Pass/Fail'] is the target column: -1 means Pass; 1 means Fail
 """)
+
+st.dataframe(secom)
