1d57054f95a84e3c27a80721267cb5e410232137
Selina <selina.chen@mdfinstruments.com>
2024-12-29 17:37:04 +0800

    Landing off and Starting this project.

---
+++ 1_uci-secom.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import kagglehub
+import numpy as np
+import plotly.express as px
+import streamlit as st
+
+#streamlit_setting
+
+st.set_page_config(layout="wide",
+                   page_title="UCI-SECOM Dataset from Kaggle",
+                   page_icon=":Chart:")
+
+#streamlit title&Time Frame
+st.title('Data Analysis for the dataset - UCI-SECOM form Kaggle.')
